71d7845a8439dfbc759006fec792c2d61c1e3e82
nj-jay <442613776@qq.com>
2020-09-03 16:09:50 +0800

    upgrade

--- uc.go
+++ uc.go
@@ -1,8 +1,46 @@
 package uc
 
 
-func HelloWorld() string {
+func Add(a ...interface{}) interface{} {
 
-    return "HelloWorld"
+    var sumInt int
 
+    var sumFloat64 float64
+
+    for index, v := range a {
+
+        switch v.(type) {
+
+        case int:
+
+            if _, ok := v.(int); ok {
+
+                sumInt += v.(int)
+
+                if len(a) == index+1 {
+
+                    return sumInt
+                }
+            }
+
+        case float64:
+
+            if _, ok := v.(float64); ok {
+
+                sumFloat64 += v.(float64)
+
+                if len(a) == index +1 {
+
+                    return sumFloat64
+                }
+            }
+
+        default:
+
+            return nil
+
+        }
+    }
+    return 0
 }
+
