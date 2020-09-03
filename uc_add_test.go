b1ad57d47dc8b1b97022984f6cd937a7122c7827
nj-jay <442613776@qq.com>
2020-09-04 00:01:41 +0800

    tag@v0.0.1

--- uc_add_test.go
+++ uc_add_test.go
@@ -8,11 +8,16 @@ import (
 func TestAdd(t *testing.T) {
 
 
-
 	if Add(1, 2) != 3 {
 
 		t.Error("Add(1, 2) must be 3")
 	}
+
+    //if Add(1.1, 2,2) != 3.3 {
+
+      //  t.Error("Add(1.1, 2.2) must be 3.3")
+
+    //}
 }
 
 
